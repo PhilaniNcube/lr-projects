739f135b8df92a2ae47beb77d9c2d2ac01fd3380
Philani Ncube <ncbphi001@gmail.com>
2024-02-10 17:29:24 +0200

    Refactor project list layout

--- app/(home)/project-list.tsx
+++ app/(home)/project-list.tsx
@@ -10,13 +10,13 @@ const ProjectList = () => {
 			<section className="my-8">
 				<div className="container p-0 text-yellow-600">
 					<h2 className="mb-4 text-3xl lg:text-5xl">Our Projects</h2>
-					<div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+					<div className="grid grid-cols-1 gap-6 px-8 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:px-0 @container">
 						{projects.map((project) => (
-							<div key={project.title} className="relative group">
+							<div key={project.title} className="relative group ">
 								<img
 									src={project.image}
 									alt={project.title}
-									className="object-cover w-full rounded-lg shadow-lg aspect-[3/6] sm:aspect-[3/4] md:aspect-[3/5] lg:aspect-[3/4] xl:aspect-[3/4]"
+									className="object-cover w-full rounded-lg shadow-lg aspect-square @sm:aspect-[3/4] @md:aspect-[3/5] @lg:aspect-[3/4] "
 								/>
 								<div className="absolute inset-0 flex flex-col justify-end p-8 text-white rounded-lg group-hover:bg-slate-950/30 bg-slate-950/60">
 									<h3 className="text-lg font-medium line-clamp-2">
@@ -24,10 +24,7 @@ const ProjectList = () => {
 									</h3>
 									<p className="my-2 text-sm line-clamp-1">{project.client}</p>
 									{/* <p className="text-sm line-clamp-2">{project.scope}</p> */}
-									<Button
-										type="button"
-										className="mt-3 bg-emerald-950 gap-x-2"
-									>
+									<Button type="button" className="mt-3 bg-emerald-950 gap-x-2">
 										<Link
 											className="flex items-center justify-center gap-x-3"
 											href={`/project/${project.id}`}
